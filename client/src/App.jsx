5851cc83cad25a0b092ef93a2ad71a3e4578c635
Ashok Babu Bavireddy <130152937+notashock@users.noreply.github.com>
2025-10-15 23:00:11 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import React, { useState } from "react";
+import LoginPage from "./pages/LoginPage";
+import WelcomePage from "./pages/WelcomePage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
+  const [user, setUser] = useState("user1");
+  const [showLogin, setShowLogin] = useState(false);
 
-  return (
-    <><div>
-      </div></>
-  )
+  if (!user) {
+    return showLogin ? (
+      <LoginPage setUser={setUser} />
+    ) : (
+      <WelcomePage setUser={() => setShowLogin(true)} />
+    );
+  }
+
+  return <ChatPage user={user} />;
 }
 
-export default App
+export default App;
