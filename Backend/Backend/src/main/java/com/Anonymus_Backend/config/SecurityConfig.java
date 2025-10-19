b60d8e5284495928b6be9fd563c7bff5be2dd2cf
Ashok Babu Bavireddy <130152937+notashock@users.noreply.github.com>
2025-10-19 17:16:48 +0530

    bug fix

--- Backend/Backend/src/main/java/com/Anonymus_Backend/config/SecurityConfig.java
+++ Backend/Backend/src/main/java/com/Anonymus_Backend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.Anonymus_Backend.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +14,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
